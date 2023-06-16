686da43e5648b6c0a666a96d5f7ee88550bf7174
danacampan <118621995+danacampan@users.noreply.github.com>
2023-06-16 16:21:55 +0300

    Update insertion2.c

--- insertion2.c
+++ insertion2.c
@@ -25,7 +25,7 @@ int main() {
     srand(time(NULL));
     n = sizeof(arr) / sizeof(arr[0]);
 
-    // initialize the array with random numbers
+   
     for (i = 0; i < n; i++) {
         arr[i] = rand() % 100;
     }
@@ -33,13 +33,13 @@ int main() {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start); // start the timer
 
-    // sort the array
+   
     insertionSort(arr, n, &compCount);
 
     clock_gettime(CLOCK_MONOTONIC, &end); // stop the timer
     double time_spent = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_nsec - start.tv_nsec) / 1000000.0;
 
-    // print the sorted array
+   
     printf("Sorted array: \n");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
